5ed6ac6e7167cef3a7e1031489fd33edccf3f96e
Pankaj Raj <pankaj.raj@oodles.io>
2022-07-08 16:50:29 +0530

    Add global exception handling.

---
+++ src/main/java/com/playground/exception/ApplicationExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.playground.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.fasterxml.jackson.databind.RuntimeJsonMappingException;
+import com.playground.utils.Response;
+
+@ControllerAdvice
+public class ApplicationExceptionHandler {
+	
+	@ExceptionHandler
+	public ResponseEntity<Object> handleBadCredentialsException(BadCredentialsException e, HttpServletRequest request) {
+		return Response.generateResponse(HttpStatus.NOT_ACCEPTABLE, null, e.getMessage(), false);
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<Object> handleRuntimeJsonMappingException(RuntimeJsonMappingException e, HttpServletRequest request) {
+		return Response.generateResponse(HttpStatus.NOT_ACCEPTABLE, null, e.getMessage(), false);
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<Object> handleUsernameNotFoundException(UsernameNotFoundException e, HttpServletRequest request) {
+		return Response.generateResponse(HttpStatus.NOT_ACCEPTABLE, null, e.getMessage(), false);
+	}
+	
+}
