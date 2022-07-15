8402a4e9c82d4a936b82650987c40d51d793e4ac
Pankaj Raj <pankaj.raj@oodles.io>
2022-07-15 15:01:59 +0530

    Add swagger.

--- src/main/java/com/playground/PlaygroundVideoBackendApplication.java
+++ src/main/java/com/playground/PlaygroundVideoBackendApplication.java
@@ -7,13 +7,27 @@ import org.springframework.web.client.RestTemplate;
 
 import com.playground.domain.OtpSessions;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
 public class PlaygroundVideoBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlaygroundVideoBackendApplication.class, args);
 	}
-
+	
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("com.playground"))
+				.build();
+	}
+	
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
