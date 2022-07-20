1e06b0191117f5b61489dfe932d4cdecb5016e32
Pankaj Raj <pankaj.raj@oodles.io>
2022-07-20 14:13:53 +0530

    Implement job scheduling for update session data.

---
+++ src/main/java/com/playground/service/SessionUpdateService.java
@@ -0,0 +1,49 @@
+package com.playground.service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.playground.domain.Session;
+import com.playground.dto.SessionPayload;
+import com.playground.repository.SessionRepository;
+
+@Service
+public class SessionUpdateService {
+	
+	@Autowired SessionRepository sessionRepository;
+	@Autowired SessionService sessionService;
+	
+	private static final Logger log = LoggerFactory.getLogger(SessionUpdateService.class);
+	
+	//@Scheduled(cron = "1 * * ? * *") //every second
+	@Scheduled(cron = "0 */5 * ? * *") // every five minutes
+	@Async
+	public void updateSessionDetails() {
+		try {
+			List<Session> listOfSession = sessionRepository.findAllBySessionStatus("LIVE");
+			listOfSession.parallelStream().forEach(data->{
+				SessionPayload sessionDetails = sessionService.fetchSessionDetails(data.getSessionUUID());
+				if (Objects.nonNull(sessionDetails)) {
+					data.setHasRecording(sessionDetails.isHas_recording());
+					data.setSessionStatus("PAST");
+					sessionRepository.save(data);
+					if (sessionDetails.isHas_recording()) {
+						sessionService.saveRecordingOfSession(data.getSessionUUID());
+					};
+				}
+			});
+			log.info("New job has executed at " + LocalDateTime.now());
+		} catch (Exception e) {
+			log.error("Issues generated in job execution time - " + e.getLocalizedMessage());
+		}
+	}
+	                                                                                              
+}
