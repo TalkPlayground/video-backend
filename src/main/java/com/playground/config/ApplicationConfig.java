b4fc71d142b574c0e79d887bbe231270e4c2f22e
Pankaj Raj <pankaj.raj@oodles.io>
2022-07-26 17:13:25 +0530

    handle rest template issues.

--- src/main/java/com/playground/config/ApplicationConfig.java
+++ src/main/java/com/playground/config/ApplicationConfig.java
@@ -1,7 +1,18 @@
 package com.playground.config;
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+
 import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.TrustStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -23,13 +34,25 @@ public class ApplicationConfig {
 				.build();
 	}
 	
+//	@Bean
+//	public RestTemplate restTemplate() {
+//	    RestTemplate restTemplate = new RestTemplate();
+//	    HttpClient httpClient = HttpClientBuilder.create().build();
+//	    HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+//	    restTemplate.setRequestFactory(requestFactory);
+//	    return restTemplate;
+//	}
+	
 	@Bean
-	public RestTemplate restTemplate() {
-	    RestTemplate restTemplate = new RestTemplate();
-	    HttpClient httpClient = HttpClientBuilder.create().build();
-	    HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-	    restTemplate.setRequestFactory(requestFactory);
-	    return restTemplate;
+	public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+		TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+		SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+		SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+		CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+		requestFactory.setHttpClient(httpClient);
+		RestTemplate restTemplate = new RestTemplate(requestFactory);
+		return restTemplate;
 	}
 	
 	@Bean
