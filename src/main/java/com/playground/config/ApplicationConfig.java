47a10242f071b08194d9b3e763de77de55512ef4
Pankaj Raj <pankaj.raj@oodles.io>
2022-07-26 17:57:36 +0530

    handle rest template issues.

--- src/main/java/com/playground/config/ApplicationConfig.java
+++ src/main/java/com/playground/config/ApplicationConfig.java
@@ -6,13 +6,17 @@ import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.TrustStrategy;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
@@ -43,17 +47,43 @@ public class ApplicationConfig {
 //	    return restTemplate;
 //	}
 	
-	@Bean
-	public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
-		TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
-		SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
-		SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
-		CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
-		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-		requestFactory.setHttpClient(httpClient);
-		RestTemplate restTemplate = new RestTemplate(requestFactory);
-		return restTemplate;
-	}
+	  @Bean
+	    public RestTemplate restTemplate(RestTemplateBuilder builder) throws NoSuchAlgorithmException, KeyManagementException {
+	        TrustManager[] trustAllCerts = new TrustManager[] {
+	                (TrustManager) new X509TrustManager() {
+	                    public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+	                        return new X509Certificate[0];
+	                    }
+	                    public void checkClientTrusted(
+	                            java.security.cert.X509Certificate[] certs, String authType) {
+	                    }
+	                    public void checkServerTrusted(
+	                            java.security.cert.X509Certificate[] certs, String authType) {
+	                    }
+	                }
+	        };  
+	        SSLContext sslContext = SSLContext.getInstance("SSL");
+	        sslContext.init(null, trustAllCerts, new java.security.SecureRandom()); 
+	        CloseableHttpClient httpClient = HttpClients.custom()
+	                .setSSLContext(sslContext)
+	                .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE)
+	                .build();   
+	        HttpComponentsClientHttpRequestFactory customRequestFactory = new HttpComponentsClientHttpRequestFactory();
+	        customRequestFactory.setHttpClient(httpClient);
+	        return builder.requestFactory(() -> customRequestFactory).build();  
+	    }
+	
+//	@Bean
+//	public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+//		TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+//		SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+//		SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+//		CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+//		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+//		requestFactory.setHttpClient(httpClient);
+//		RestTemplate restTemplate = new RestTemplate(requestFactory);
+//		return restTemplate;
+//	}
 	
 	@Bean
 	public OtpSessions otpSessions() {
