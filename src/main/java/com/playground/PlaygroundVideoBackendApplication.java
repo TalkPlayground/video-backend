08606f5174129a531e6eb7f700a3325962241b7d
sumitkashyap079 <sumit.kashyap@oodles.io>
2022-08-18 15:32:26 +0530

    Some changes on main methods

--- src/main/java/com/playground/PlaygroundVideoBackendApplication.java
+++ src/main/java/com/playground/PlaygroundVideoBackendApplication.java
@@ -16,34 +16,34 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class PlaygroundVideoBackendApplication {
 
     public static void main(String[] args) {
-        SSLContextHelper.setSslProperties();
+//        SSLContextHelper.setSslProperties();
         SpringApplication.run(PlaygroundVideoBackendApplication.class, args);
     }
 
-    protected static class SSLContextHelper {
-        private static final Logger logger = LoggerFactory.getLogger(PlaygroundVideoBackendApplication.class);
-        private static final String SSL_TRUST_STORE = "javax.net.ssl.trustStore";
-        private static final String SSL_TRUST_STORE_PASSWORD = "javax.net.ssl.trustStorePassword";
-        private static final String SSL_TRUST_STORE_TYPE = "javax.net.ssl.trustStoreType";
-        private static final String KEY_STORE_TYPE = "JKS";
-        private static final String DEFAULT_KEY_STORE_PASSWORD = "playground";
-        private static final String DEFAULT_KEYSTORE = "/app/rds-truststore.jks";
-        private static final String SSL_KEYSTORE = "sslKeyStore";
-
-        private static void setSslProperties() {
-            try {
-                String sslKeyStore = System.getProperty(SSL_KEYSTORE);
-                logger.info("-DsslKeyStore={}", sslKeyStore);
-                if (StringUtils.isEmpty(sslKeyStore)) {
-                    sslKeyStore = DEFAULT_KEYSTORE;
-                }
-                logger.info("ssl keystore path: {}", sslKeyStore);
-                System.setProperty(SSL_TRUST_STORE, sslKeyStore);
-                System.setProperty(SSL_TRUST_STORE_TYPE, KEY_STORE_TYPE);
-                System.setProperty(SSL_TRUST_STORE_PASSWORD, DEFAULT_KEY_STORE_PASSWORD);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//    protected static class SSLContextHelper {
+//        private static final Logger logger = LoggerFactory.getLogger(PlaygroundVideoBackendApplication.class);
+//        private static final String SSL_TRUST_STORE = "javax.net.ssl.trustStore";
+//        private static final String SSL_TRUST_STORE_PASSWORD = "javax.net.ssl.trustStorePassword";
+//        private static final String SSL_TRUST_STORE_TYPE = "javax.net.ssl.trustStoreType";
+//        private static final String KEY_STORE_TYPE = "JKS";
+//        private static final String DEFAULT_KEY_STORE_PASSWORD = "playground";
+//        private static final String DEFAULT_KEYSTORE = "rds-truststore.jks";
+//        private static final String SSL_KEYSTORE = "sslKeyStore";
+//
+//        private static void setSslProperties() {
+//            try {
+//                String sslKeyStore = System.getProperty(SSL_KEYSTORE);
+//                logger.info("-DsslKeyStore={}", sslKeyStore);
+//                if (StringUtils.isEmpty(sslKeyStore)) {
+//                    sslKeyStore = DEFAULT_KEYSTORE;
+//                }
+//                logger.info("ssl keystore path: {}", sslKeyStore);
+//                System.setProperty(SSL_TRUST_STORE, sslKeyStore);
+//                System.setProperty(SSL_TRUST_STORE_TYPE, KEY_STORE_TYPE);
+//                System.setProperty(SSL_TRUST_STORE_PASSWORD, DEFAULT_KEY_STORE_PASSWORD);
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 }
